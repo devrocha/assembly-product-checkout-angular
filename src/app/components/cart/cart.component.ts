50c7bba5d4a5dc1b23e89a1f19e2ac48aa402d80
Alexandre Dias <alexandrealmeidadias@gmail.com>
2025-09-11 21:43:33 +0100

    filtros

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, Signal } from '@angular/core';
 import { Products } from '../../../services/products';
 import { CommonModule } from '@angular/common';
 
@@ -14,25 +14,6 @@ export class CartComponent {
 
   protected products = this.productService.getProducts();
 
-  private itemsPerPage = 25;
-
-  private totalItems = this.products.length;
-
-  protected totalPages = Math.ceil(this.totalItems / this.itemsPerPage);
-
-  protected pages: number[] = [];
-
-  protected filterText = '';
-
-  constructor() {
-    for (let i = 1; i <= this.totalPages; i++) {
-      this.pages.push(i);
-    }
-
-  }
-
-  protected currentPage = 1;
-
   protected selectedProducts = this.products;
 
   protected paginatedProducts() {
@@ -41,15 +22,35 @@ export class CartComponent {
     return this.selectedProducts.slice(startIndex, endIndex);
   }
 
+  private itemsPerPage = 25;
+
+  private totalItems = signal(this.selectedProducts.length);
+
+  protected totalPages = signal(Math.ceil(this.totalItems() / this.itemsPerPage));
+
+  protected pages: number[] = [];
+
+  protected filterText = '';
+
+  constructor() {
+    for (let i = 1; i <= this.totalPages(); i++) {
+      this.pages.push(i);
+    }
+
+  }
+
+  protected currentPage = 1;
+
   protected goToPage(page: number) {
-    if (page >= 1 && page <= this.totalPages) {
+    if (page >= 1 && page <= this.totalPages()) {
       this.currentPage = page;
     }
   }
 
   protected addFilter(value: string) {
     this.filterText = value;
-    this.selectedProducts = this.products.filter(p => p.name.toLowerCase().includes(this.filterText.toLowerCase()));
+    this.selectedProducts = this.products.filter(p => p.name.toLowerCase().includes(this.filterText.toLowerCase())
+      || p.category.toLowerCase().includes(this.filterText.toLowerCase()));
   }
 }
 
