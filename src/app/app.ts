d9cc845487050f109e287dbc6a1c16feb48620ff
Andre Almeida <afmalmeida97@gmail.com>
2025-09-11 21:04:17 +0100

    paginacao

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,36 @@
-import { Component, inject, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal, computed } from '@angular/core';
 import { Products } from '../services/products';
 import { CommonModule } from '@angular/common';
+import { routes } from './app.routes';
 
 @Component({
   selector: 'app-root',
-  imports: [ CommonModule],
+  imports: [ routes , CommonModule ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  productsService = inject(Products)
+  productsService = inject(Products);
 
-  protected products = this.productsService.getProducts()
+  protected products = this.productsService.getProducts();
 
-  constructor(){
-    console.log(this.products)
+  page = signal(1);
+  pageSize = 25;
+
+  pagedProducts = computed(() => {
+    const start = (this.page() - 1) * this.pageSize;
+    return this.products.slice(start, start + this.pageSize);
+  });
+
+  nextPage() {
+    if ((this.page() * this.pageSize) < this.products.length) {
+      this.page.update(p => p + 1);
+    }
+  }
+
+  prevPage() {
+    if (this.page() > 1) {
+      this.page.update(p => p - 1);
+    }
   }
 }
