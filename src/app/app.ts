eb09b9c1c5c50aa1f0510c79751c29bde56b1957
BrunoRoma90 <brunoroma.90@gmail.com>
2025-09-11 20:56:47 +0100

    render products

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { Products } from '../services/products';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
