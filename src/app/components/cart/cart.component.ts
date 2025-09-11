f4cb90520dde8b86ec8346878b53d3cbbb09f607
Alexandre Dias <alexandrealmeidadias@gmail.com>
2025-09-11 20:53:22 +0100

    paginacao

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -13,4 +13,34 @@ export class CartComponent {
   private productService = inject(Products);
 
   protected products = this.productService.getProducts();
+
+  private itemsPerPage = 25;
+
+  private totalItems = this.products.length;
+
+  protected totalPages = Math.ceil(this.products.length / this.itemsPerPage);
+  protected pages: number[] = [];
+
+  constructor() {
+    for (let i = 1; i <= this.totalPages; i++) {
+      this.pages.push(i);
+    }
+
+  }
+
+  protected currentPage = 1;
+
+
+  protected paginatedProducts() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.products.slice(startIndex, endIndex);
+  }
+
+  protected goToPage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+    }
+  }
 }
+
